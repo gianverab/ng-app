eb728d795e7d0ac47dc025a7303f4bc12ea67524
Giancarlo Vera <paulterry2001@hotmail.com>
2016-08-25 11:08:08 -0300

    Add remain HTML templates with their controllers

--- source/js/app.js
+++ source/js/app.js
@@ -6,10 +6,31 @@ var app = angular.module("test", ['ngRoute'])
             templateUrl: 'main.html',
             controller: 'MainCtrl'
         }).
+        when('/contact', {
+            templateUrl: 'contact.html',
+            controller: 'ContactCtrl'
+        }).
+        when('/services', {
+            templateUrl: 'services.html',
+            controller: 'ServiceCtrl'
+        }).
+        when('/pricing', {
+            templateUrl: 'pricing.html',
+            controller: 'PricingCtrl'
+        }).
         otherwise({redirectTo: '/main'})
 }])
 .controller('MainCtrl', ['$scope', function ($scope){
     $scope.person = 'Gian Vera';
     $scope.job = 'Frontend Developer';
+    console.log($scope);
+}])
+.controller('ServiceCtrl', ['$scope', function ($scope){
+    console.log($scope);
+}])
+.controller('PricingCtrl', ['$scope', function ($scope){
+    console.log($scope);
+}])
+.controller('ContactCtrl', ['$scope', function ($scope){
     console.log($scope);
 }]);
